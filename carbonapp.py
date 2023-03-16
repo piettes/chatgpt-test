acecd113383f238931288146a6aff8a6d94bd853
p00920649 <p00920649@s2141886.bdr.de>
2023-03-16 11:14:45 +0100

    fix codestyle

--- carbonapp.py
+++ carbonapp.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, jsonify
 import random
 
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/graph')
 def graph():
@@ -15,7 +18,6 @@ def graph():
 # Endpoint that returns data in JSON format
 @app.route('/data')
 def get_data():
-
     data = {
         'labels': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
         'values': [100, 200, 300, 400, 500, 600],
@@ -25,12 +27,16 @@ def get_data():
 
     return jsonify(data)
 
+
 @app.route('/points')
 def get_points():
     points = []
     names = ["Point A", "Point B", "Point C", "Point D", "Point E", "Point F"]
-    short_descs = ["This is point A", "This is point B", "This is point C", "This is point D", "This is point E", "This is point F"]
-    long_descs = ["This is a longer description for point A", "This is a longer description for point B", "This is a longer description for point C", "This is a longer description for point D", "This is a longer description for point E", "This is a longer description for point F"]
+    short_descs = ["This is point A", "This is point B", "This is point C", "This is point D", "This is point E",
+                   "This is point F"]
+    long_descs = ["This is a longer description for point A", "This is a longer description for point B",
+                  "This is a longer description for point C", "This is a longer description for point D",
+                  "This is a longer description for point E", "This is a longer description for point F"]
     for i in range(6):
         point = {}
         point['label'] = names[i]
@@ -50,16 +56,16 @@ def calculate():
 
     # Calculate carbon footprint based on user inputs
     if transportation == 'driving':
-        transportation_footprint = 0.43 # kg CO2 per km driven
+        transportation_footprint = 0.43  # kg CO2 per km driven
     elif transportation == 'public_transport':
-        transportation_footprint = 0.04 # kg CO2 per km taken
+        transportation_footprint = 0.04  # kg CO2 per km taken
     elif transportation == 'walking':
-        transportation_footprint = 0 # kg CO2 per km walked or biked
+        transportation_footprint = 0  # kg CO2 per km walked or biked
 
     if food == 'meat':
-        food_footprint = 6.6 # kg CO2 per kg of beef consumed
+        food_footprint = 6.6  # kg CO2 per kg of beef consumed
     elif food == 'vegetarian':
-        food_footprint = 2 # kg CO2 per kg of tofu consumed
+        food_footprint = 2  # kg CO2 per kg of tofu consumed
 
     if home_energy == 'electricity':
         home_energy_footprint = 0.527  # kg CO2 per kWh of electricity used
@@ -94,9 +100,9 @@ def calculate():
 
     # Calculate total carbon footprint per year
     total_footprint = (transportation_footprint * average_daily_driving +
-                        # walking_footprint * average_daily_walking +
-                        food_footprint * average_daily_food_consumption +
-                        home_energy_footprint * average_home_energy_use) / 1000
+                       # walking_footprint * average_daily_walking +
+                       food_footprint * average_daily_food_consumption +
+                       home_energy_footprint * average_home_energy_use) / 1000
     carbon_footprint_per_year = total_footprint * days_in_year
 
     # This assumes an average of 13 km driving per day, 3 km walking per day, 1.5 kg of food consumed per day,
@@ -105,10 +111,11 @@ def calculate():
     # and then multiplied by days_in_year to get the carbon_footprint_per_year.
     # Calculate total carbon footprint
 
-    #total_footprint = transportation_footprint * 10 + food_footprint * 2 + home_energy_footprint * 1000 # assuming 10 km per day, 2 meals per day, and 1000 kWh per year
+    # total_footprint = transportation_footprint * 10 + food_footprint * 2 + home_energy_footprint * 1000 # assuming 10 km per day, 2 meals per day, and 1000 kWh per year
 
     # Render template with results
     return render_template('results.html', total_footprint=carbon_footprint_per_year)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
